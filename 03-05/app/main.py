df459dc79e5317ac06bfd0c92e4d98139a826e60
Naila_Nay01 <111473823+01juni1999@users.noreply.github.com>
2022-09-05 11:52:35 +0700

    Add files via upload

--- 03-05/app/main.py
+++ 03-05/app/main.py
@@ -1,8 +1,8 @@
-import imp
-from tkinter.messagebox import QUESTION
 from flask import Flask
-from flask import render_template, request
+from flask import render_template
+from flask import request, redirect
 import psycopg2
+
 app = Flask(__name__)
 
 @app.route("/",methods=["GET", "POST"])
@@ -14,20 +14,12 @@ def index():
         password="PPQS0106"
     )
     curs = conn. cursor()
-
-    if request.method =="POST":
-        nama=request.form.get ("nama")
-        detail=request.form.get("detail")
-        
-        # print(40*"=")
-        # print(nama)
-        # print(detail)
-        # print(40*"=")
-    query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
-    curs.execute(query)
-    conn.commit()
-    curs.close()
-    conn.close()
+    if request.method == "POST":
+        nama=request.form["nama"]
+        detail=request.form["detail"]
+        query = f"insert into buah(nama, detail) values('{nama}', '{detail}')"
+        curs.execute(query)
+        conn.commit()
         
     print(request.method)
     query = f"select * from buah"
@@ -35,8 +27,47 @@ def index():
     data = curs.fetchall()
     curs.close()
     conn.close()
-    # data = ["aple", "pear", "anggur"]
+     # data = ["aple", "pear", "anggur"]
     return render_template("index.html", context=data)
+
+
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="PPQS0106"
+    )
+
+    curs = conn.cursor()
+    query = f"select * from buah where id = {buah_id}"
+    curs.execute(query)
+    data = curs.fetchall()
+    curs.close()
+    conn.close()
+    print(data)
+    return render_template("detail.html", context=data)
+
+
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+        host="localhost",
+        database="contoh",
+        user="postgres",
+        password="PPQS0106"
+    )
+
+    curs = conn.cursor()
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect("/")
+    
+   
 
 if __name__ == "__main__":
         app.run()
